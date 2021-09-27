9a70ee4daf84e0582888886dc01979811852ff49
Charles C <chcalzia@yahoo.com>
2021-09-27 22:45:34 +0100

    Added listExercises

---
+++ listExercises/listExercises/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int Sum(List <int> list)
+        {
+            int sum = 0;
+            foreach (int i in list)
+                sum += i;
+            return sum;
+        }
+    }
+}
